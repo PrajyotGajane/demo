10c0122502721534cd551868fbfac962f8805491
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-13 22:03:51 +0530

    Hello.java added

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 class Hello{
         public static void main(String[] args){
-<<<<<<< HEAD
         System.out.print("Changes at local repo");
-=======
-        System.out.print("Changes at remote master");
->>>>>>> 1c0f10ae542000b99f79a5094d47f4cdb3d63310
         }
 }
