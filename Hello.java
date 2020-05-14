05e63dc367cc931f9f50d0065a4bcbbf81b93d54
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-14 08:54:20 +0530

    Chenges made in dev

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
         public static void main(String[] args){
         System.out.println("Changes at local repo");
-	System.out.println("in dev branch");
+	System.out.println("Changes at dev branch");
         }
 }
