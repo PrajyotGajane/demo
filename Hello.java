29c5d8cc34e6c49cc0337b4596927a594c58e734
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-14 09:15:51 +0530

    Change at dev branc

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
         public static void main(String[] args){
         System.out.println("Changes at local repo");
-	System.out.println("Changes at dev branch");
+	System.out.println("Changes again at mastter branch");
         }
 }
